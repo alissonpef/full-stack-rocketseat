8fb04235795b976c980825845daac115214ee426
alissonpef <alissonpef@gmail.com>
2026-06-09 18:51:16 -0300

    feat: implement Zod schema validation for product creation and add global error handling for Zod validation errors

--- 2-API-Rest/src/controllers/products-controller.ts
+++ 2-API-Rest/src/controllers/products-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AppError } from "../utils/app-error";
+import { z } from "zod";
 
 class ProductsController {
   /*
@@ -17,14 +18,18 @@ class ProductsController {
   }
 
   create(req: Request, res: Response) {
-    const { name, price } = req.body;
+    const bodySchema = z.object({
+      name: z
+        .string({ required_error: "Name is required!" })
+        .trim()
+        .min(6, { message: "Name must be 6 or more characteres" }),
+      price: z
+        .number({ required_error: "Price is required!" })
+        .positive({ message: "Price must be positive!" })
+        .nullish(),
+    });
 
-    if (!name) {
-        throw new AppError("Nome do produto é obrigatório!")
-    }
-    if (!price) {
-        throw new AppError("Preço do produto é obrigatório!")
-    }
+    const { name, price } = bodySchema.parse(req.body);
 
     // res.send(`Produto ${name} custa ${price}`)
     res.status(201).json({ name, price, user_id: req.user_id });
--- 2-API-Rest/src/server.ts
+++ 2-API-Rest/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 import { AppError } from "./utils/app-error";
+import { ZodError } from "zod"
 
 const PORT = 3333;
 
@@ -12,6 +13,10 @@ app.use(routes);
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return res.status(400).json({ message: "Validation error!", issues: error.format()});
   }
 
   res.status(500).json({ message: error.message });
