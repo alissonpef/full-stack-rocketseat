58d0edc06674671de321b8875b7b228c4e93c5cc
alissonpef <alissonpef@gmail.com>
2026-07-12 20:34:14 -0300

    feat(react): initialize counter application project
    - add react application with counter functionality
    - create reusable button component with css modules
    - implement state management for increment and decrement logic
    - add custom hook for message logging and global styling configuration

---
+++ 09_react/src/App.tsx
@@ -0,0 +1,32 @@
+import "./global.css";
+import { useState, useEffect } from "react";
+import { Button } from "./components/button";
+import styles from "./app.module.css";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    setCount((prevState) => prevState + 1);
+  }
+
+  function handleRemove() {
+    if (count > 0) {
+      setCount((prevState) => prevState - 1);
+    }
+  }
+
+  useEffect(() => {
+    if (count > 0) {
+      console.log("count change: ", +count);
+    }
+  }, [count]);
+
+  return (
+    <div className={styles.container}>
+      <Button name="Adicionar" onClick={handleAdd} />
+      <span>{count}</span>
+      <Button name="Excluir" onClick={handleRemove} />
+    </div>
+  );
+}
