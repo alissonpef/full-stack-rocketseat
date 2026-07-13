32f9a2c1495234ea2dffcf65a0749ee39227380a
alissonpef <alissonpef@gmail.com>
2026-07-13 15:56:22 -0300

    feat(app): implement refund request system
    - create authentication context and hooks for session management
    - implement protected routing based on user roles (employee/manager)
    - add refund request and dashboard pages with HTTP integration
    - implement form validation using Zod and API interaction via Axios
    - create reusable UI components including buttons, inputs, and modals

---
+++ 13_requisicao_http/src/pages/SignIn.tsx
@@ -0,0 +1,78 @@
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import { useActionState } from "react";
+import { z, ZodError } from "zod";
+import { api } from "../services/api";
+import { AxiosError } from "axios";
+import { useAuth } from "../hooks/useAuth";
+
+const signInSchema = z.object({
+  email: z.string().email({ message: "E-mail inválido" }),
+  password: z.string().trim().min(1, { message: "Informe sua senha" }),
+});
+
+export function SignIn() {
+  const [state, formAction, isLoading] = useActionState(signIn, null);
+
+  const auth = useAuth();
+
+  async function signIn(_: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
+
+      const response = await api.post("/sessions", data);
+      auth.save(response.data);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return { message: error.issues[0].message };
+      }
+
+      if (error instanceof AxiosError) {
+        return { message: error.response?.data.message };
+      }
+
+      return { message: "Erro ao fazer login" };
+    }
+  }
+
+  return (
+    <form action={formAction} className="w-full flex flex-col gap-4">
+      <Input
+        required
+        name="email"
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
+
+      <Input
+        required
+        name="password"
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+      />
+
+      <p className="text-red-600 text-sm text-center my-4 font-medium">
+        {state?.message}
+      </p>
+
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+
+      <a
+        href="/signUp"
+        className="text-sm font-semibold text-gray-100 mt-10
+      mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar conta
+      </a>
+    </form>
+  );
+}
---
+++ 13_requisicao_http/src/pages/SignUp.tsx
@@ -0,0 +1,111 @@
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import { useState } from "react";
+import { z, ZodError } from "zod";
+import { api } from "../services/api";
+import { useNavigate } from "react-router";
+import { AxiosError } from "axios";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+    confirmPassword: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não conferem",
+    path: ["confirmPassword"],
+  });
+
+export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso! Deseja fazer login?")) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possível cadastrar o usuário!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="Nome"
+        placeholder="Digite seu nome"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="Confirme a Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Cadastrar
+      </Button>
+
+      <a
+        href="/"
+        className="text-sm font-semibold text-gray-100 mt-10
+      mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Já tenho uma conta
+      </a>
+    </form>
+  );
+}
